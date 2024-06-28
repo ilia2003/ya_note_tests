c65a005e1e4ac8f19ba16920b55b511d4769cd68
borodinskiy <anton.borodinskiy@mail.ru>
2024-06-29 00:15:31 +0300

    1

---
+++ notes/pytest_tests/conftest.py
@@ -0,0 +1,64 @@
+# conftest.py
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from notes.models import Note
+# Импортируем функции для проверки редиректа и ошибки формы:
+from pytest_django.asserts import assertRedirects, assertFormError
+
+# Импортируем из модуля forms сообщение об ошибке:
+from notes.forms import WARNING
+
+
+@pytest.fixture
+def form_data():
+    return {
+        'title': 'Новый заголовок',
+        'text': 'Новый текст',
+        'slug': 'new-slug'
+    }
+@pytest.fixture
+# Используем встроенную фикстуру для модели пользователей django_user_model.
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):  # Вызываем фикстуру автора.
+    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+    return client
+
+
+@pytest.fixture
+def note(author):
+    note = Note.objects.create(  # Создаём объект заметки.
+        title='Заголовок',
+        text='Текст заметки',
+        slug='note-slug',
+        author=author,
+    )
+    return note
+
+@pytest.fixture
+# Фикстура запрашивает другую фикстуру создания заметки.
+def slug_for_args(note):
+    # И возвращает кортеж, который содержит slug заметки.
+    # На то, что это кортеж, указывает запятая в конце выражения.
+    return (note.slug,)
